66839a7038a11158cc7b93dd7e218fb9503f670b
Sakshi Agrawal <sakshiagrawal709@gmail.com>
2021-10-13 11:27:01 +0530

    react-build-upload

--- backend/app.js
+++ backend/app.js
@@ -30,12 +30,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/scheme',schemeRouter);
 app.use('/upload',uploadRouter);
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
